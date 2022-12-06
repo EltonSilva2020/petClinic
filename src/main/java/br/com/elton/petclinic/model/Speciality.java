a7ba612223c20a3e023440bfd5b2f2492636d7cf
EltonSilva <elto_bs@hotmail.com>
2022-12-06 19:36:56 +0100

    feat:data-model. Close #11

--- src/main/java/br/com/elton/petclinic/model/Speciality.java
+++ src/main/java/br/com/elton/petclinic/model/Speciality.java
@@ -1,4 +1,19 @@
 package br.com.elton.petclinic.model;
 
-public class Speciality {
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "specialties")
+public class Speciality extends AbstractModel {
+
+    @Column(name = "description", length = 255, nullable = false)
+    private String description;
+
 }
