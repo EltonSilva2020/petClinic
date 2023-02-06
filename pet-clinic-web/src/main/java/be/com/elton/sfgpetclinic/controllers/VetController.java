8612e21b310d007186021f88ac3c8b17e7590d1f
EltonSilva <elto_bs@hotmail.com>
2023-02-06 20:17:37 +0100

    feature: Add Vet List and update controller,  closes #22

--- pet-clinic-web/src/main/java/be/com/elton/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/be/com/elton/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package be.com.elton.sfgpetclinic.controllers;
 
+import be.com.elton.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping({"vets"})
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String listVet() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVet(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
