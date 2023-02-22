74c8fc76cc91afae9d602ed30a4ab84b467c46f5
EltonSilva <elto_bs@hotmail.com>
2023-02-22 21:21:06 +0100

    feature: Add spring data jpa service for vet, closes #34

--- pet-clinic-data/src/main/java/be/com/elton/sfgpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/be/com/elton/sfgpetclinic/service/OwnerService.java
@@ -1,9 +1,11 @@
 package be.com.elton.sfgpetclinic.service;
 
 import be.com.elton.sfgpetclinic.model.Owner;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
